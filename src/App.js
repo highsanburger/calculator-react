c4b1a7fb42625c1cb84db4e8341384d5a9178a64
Syed Khalid <magmastar167@gmail.com>
2023-10-12 11:54:34 +0300

    🌛

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  const buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+',
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <div className="calculator">
+        <div className="input">
+          {input}
+        </div>
+        <div className="result">
+          {result}
+        </div>
+        <div className="buttons">
+          {buttons.map((button, index) => (
+            <button
+              key={index}
+              onClick={() => handleButtonClick(button)}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
