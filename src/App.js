95510fad62374c27573e77fe1b725073bf3ab02e
Syed Khalid <magmastar167@gmail.com>
2023-10-14 13:04:34 +0300

    work

--- src/App.js
+++ src/App.js
@@ -3,49 +3,62 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+  const [output, setOutput] = useState('0');
 
-  const handleButtonClick = (value) => {
+  const handleClick = (value) => {
     if (value === '=') {
       try {
-        setResult(eval(input));
+        setOutput(eval(input));
       } catch (error) {
-        setResult('Error');
+        setOutput('Error');
       }
     } else if (value === 'C') {
       setInput('');
-      setResult('');
+      setOutput('0');
+    } else if (value === '⌫') {
+      setInput(input.slice(0, -1));
+      setOutput(input.slice(0, -1));
     } else {
-      setInput((prevInput) => prevInput + value);
+      setInput(input + value);
+      setOutput(input + value);
     }
   };
 
-  const buttons = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-',
-    'C', '0', '=', '+',
-  ];
-
   return (
-    <div className="App">
-      <h1>React Calculator</h1>
+    <div className="app">
       <div className="calculator">
-        <div className="input">
-          {input}
-        </div>
-        <div className="result">
-          {result}
+        <div className="display">
+          <div className="output">{output}</div>
         </div>
         <div className="buttons">
-          {buttons.map((button, index) => (
-            <button
-              key={index}
-              onClick={() => handleButtonClick(button)}
-            >
-              {button}
-            </button>
-          ))}
+          <div className="row">
+            <button onClick={() => handleClick('7')}>7</button>
+            <button onClick={() => handleClick('8')}>8</button>
+            <button onClick={() => handleClick('9')}>9</button>
+            <button onClick={() => handleClick('+')}>+</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleClick('4')}>4</button>
+            <button onClick={() => handleClick('5')}>5</button>
+            <button onClick={() => handleClick('6')}>6</button>
+            <button onClick={() => handleClick('-')}>-</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleClick('1')}>1</button>
+            <button onClick={() => handleClick('2')}>2</button>
+            <button onClick={() => handleClick('3')}>3</button>
+            <button onClick={() => handleClick('*')}>x</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleClick('0')}>0</button>
+            <button onClick={() => handleClick('.')}>.</button>
+            <button onClick={() => handleClick('⌫')}>⌫</button>
+            <button onClick={() => handleClick('/')}>/</button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleClick('C')} className="clear">Clear</button>
+            <button onClick={() => handleClick('=')}>=</button>
+          </div>
         </div>
       </div>
     </div>
